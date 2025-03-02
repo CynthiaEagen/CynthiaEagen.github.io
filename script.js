682ece5a53682b502f2604fe3dd6365c0989d868
cynthia.eagen <eagen.cynthia@gmail.com>
2025-03-02 02:59:54 -0500

    Finished content, adding significant styling and a touch of js.
    Added multiple SVGs, a pdf, and a png.

--- script.js
+++ script.js
@@ -1,10 +1,30 @@
 function getYearsSkating() {
-    document.getElementById("years-skating").innerText = new Date().getFullYear() - 2004; 
+    let elements = document.getElementsByClassName("years-skating");
+    for (i = 0; i < elements.length; i++)
+        elements.item(i).innerText = new Date().getFullYear() - 2004;
 }
 
 function getYearsCoaching() {
-    document.getElementById("years-coaching").innerText = new Date().getFullYear() - 2016;
+    let elements = document.getElementsByClassName("years-coaching");
+    for (i = 0; i < elements.length; i++)
+        elements.item(i).innerText = new Date().getFullYear() - 2016;
 }
 
 getYearsSkating();
 getYearsCoaching();
+
+const resumeButton = document.getElementById("resume-button");
+const resumeContainer = document.getElementById("resume-container");
+
+resumeButton.addEventListener('click', () => { 
+    if (resumeButton.classList.contains('collapsed')) {
+        resumeButton.innerText = "Wanna see my resume?";
+    }
+    else if (!resumeButton.classList.contains('collapsed')) {
+        resumeButton.innerText = "Seen enough?";
+        setTimeout(() => {
+            document.getElementById('contact-info').scrollIntoView();
+        }, 250)
+        
+    }
+})
